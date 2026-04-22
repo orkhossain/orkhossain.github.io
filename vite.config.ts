5399e609010707ae458d5f19749930cafcb908fa
Ork Hossain Muntaqin <orkhoss.2011@gmail.com>
2026-04-22 23:51:38 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,6 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
